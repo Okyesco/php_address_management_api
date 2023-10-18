6f74bd089b39c18817f434902b733bfdcb6a7dc3
Okyesco <okyesco0549677744@gmail.com>
2023-10-18 12:56:05 +0000

    Moved auth folder into the api folder

--- api/auth/signup.php
+++ api/auth/signup.php
@@ -1,6 +1,6 @@
 <?php
-    require_once '../includes/config.php';
-    require_once '../core/account.php';
+    require_once '../../includes/config.php';
+    require_once '../../core/account.php';
 
     header("Access-Control_Allow-Origin: *");
     header("Content-Type: application/json");
