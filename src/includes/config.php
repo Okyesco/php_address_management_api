b9d3d26419a3792918475430cbf56370297c2792
Okyesco <okyesco0549677744@gmail.com>
2023-10-24 14:21:55 +0000

    Added credentials for localhost and docker db login instance

--- src/includes/config.php
+++ src/includes/config.php
@@ -1,9 +1,16 @@
 <?php
- 
+    // #Docker
     $database_host = "db";
     $database_username = "root";
     $database_password = "root";
     $database_name = "address_management_app";
+
+    #LOCALHOST
+    // $database_host = "127.0.0.1";
+    // $database_username = "root";
+    // $database_password = "";
+    // $database_name = "address_management_app";
+
     
     $mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
     
